129cf88a4ced068e81d50692d2540f46d41facad
Mayank Saxena <148477562+MayankSaxena2214@users.noreply.github.com>
2024-10-01 21:21:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { UserContextProvider } from './context/UserContext.jsx'
-export const server=`http://localhost:7000`
+export const server=`https://imageassignment-backend.onrender.com`
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserContextProvider>
